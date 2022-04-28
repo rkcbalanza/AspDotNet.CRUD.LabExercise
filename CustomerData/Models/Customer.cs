cb10eb0c27c6b1f5b75436eaa833fab772da1b05
rkcbalanza <richard.balanza@pointwest.com.ph>
2022-04-28 17:22:36 +0800

    Add project files.

---
+++ CustomerData/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerData.Models
+{
+    public class Customer : BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+    }
+}
---
+++ CustomerWeb/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using CustomerData.Models;
+using CustomerData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace CustomerWeb.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ICustomerRepository _customerRepository;
+
+        public CustomerController (ICustomerRepository customerRepository)
+        {
+            this._customerRepository = customerRepository;
+        }
+        public IActionResult Index()
+        {
+            var customerList = this._customerRepository.FindAll().ToList();
+            return View(customerList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var opera = this._customerRepository.FindByPrimaryKey(id);
+            ViewData["Opera"] = opera;
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var opera = this._customerRepository.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult New()
+        {
+            ViewData["Action"] = "New";
+            return View("Form", new Customer());
+        }
+
+        public IActionResult Edit(int id)
+        {
+            ViewData["Action"] = "Edit";
+            var opera = this._customerRepository.FindByPrimaryKey(id);
+            return View("Form", opera);
+        }
+        public IActionResult Save(string action, Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                if (action.ToLower().Equals("new"))
+                {
+                    this._customerRepository.Insert(customer);
+                }
+                else if (action.ToLower().Equals("edit"))
+                {
+                    this._customerRepository.Update(customer);
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", customer);
+            }
+        }
+    }
+}
